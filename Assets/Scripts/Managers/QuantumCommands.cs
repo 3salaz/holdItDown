dfc7d4ac59048acae24c174000a838ca6d976525
Erik <3salaz.dev@gmail.com>
2020-09-03 13:11:28 -0700

    added vendors and test folders for continuous integration

---
+++ Assets/Scripts/Managers/QuantumCommands.cs
@@ -0,0 +1,12 @@
+﻿using QFSW.QC;
+using UnityEngine;
+public class QuantumCommands
+{
+    [Command(aliasOverride: "log")]
+    [Command(aliasOverride: "print")]
+    [CommandDescription("Logs a message to the console")]
+    public static void Log(string message)
+    {
+        Debug.Log(message);
+    }
+}
